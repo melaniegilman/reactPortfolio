5b95f49c633d29e0188abad1406e04b26e792cb8
Melanie Gilman <melaniegilman259@yahoo.com>
2020-11-04 13:06:06 -0500

    will this work as far as downloading?

--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,41 +1,44 @@
 import React from 'react';
-// import melanie_resume from '../../assets/melanie_resume.pdf';
+import melanie_resume from '../../assets/melanie_resume.pdf';
 
 function Resume() {
     return (
-        <div id="resume-card" className="card m-5" style={{border: "none"}}>
+        <div id="resume-card" className="card m-5" style={{ border: "none" }}>
             <div className="row">
                 <ul className="list-unstyled col-12 col-md-4" >
                     <li className="h2">Front-End Proficiencies</li>
-                        <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>jQuery</li>
-                            <li>React</li>
-                            <li>Bootstrap</li>
-                        </ul>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>React</li>
+                        <li>Bootstrap</li>
+                    </ul>
                 </ul>
                 <ul className="list-unstyled col-12 col-md-4" >
                     <li className="h2">Back-End Proficiencies</li>
-                        <ul>
-                            <li>APIs</li>
-                            <li>Node</li>
-                            <li>Express</li>
-                            <li>MySQL, Sequelize</li>
-                            <li>MongoDB, Mongoose</li>
-                        </ul>
+                    <ul>
+                        <li>APIs</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL, Sequelize</li>
+                        <li>MongoDB, Mongoose</li>
+                    </ul>
                 </ul>
 
-                <div id="resume-download" className="text-center col-11 col-md-4" style={{border: "none"}}>
+                <div id="resume-download" className="text-center col-11 col-md-4" style={{ border: "none" }}>
                     <h2>Click here to download my Full Resume</h2>
-                    <a href={require("../../assets/melanie_resume.pdf")} download>
-                    {/* <a href="../../assets/melanie_resume.pdf" src={melanie_resume} download> */}
-                    <i className="far fa-file-pdf fa-7x mx-auto" style={{width: "100px"}}></i>
-                    </a>
+                    <section id="resume">
+            Download my full <a href="https://github.com/melaniegilman/reactPortfolio/raw/master/src/assets/melanie_resume.pdf" class="link">resume</a>
+        </section>
+                    {/* <a href={require("../../assets/melanie_resume.pdf")} download></a>
+                        <a href="../../assets/melanie_resume.pdf" src={melanie_resume} download>
+                            <i className="far fa-file-pdf fa-7x mx-auto" style={{ width: "100px" }}></i>
+                        </a> */}
+                    </div>
+                 </div>
                 </div>
-            </div>
-        </div>
     )
 }
 
