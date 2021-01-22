336bb1709b888cffcf3dd10452cc9959db83b599
Melanie Gilman <melaniegilman259@yahoo.com>
2021-01-22 12:13:31 -0500

    add project:

--- react-portfolio/src/components/Carousel/index.js
+++ react-portfolio/src/components/Carousel/index.js
@@ -3,6 +3,7 @@ import ask from '../../assets/img/ask.png'
 import blog from '../../assets/img/blog.png'
 import budget from '../../assets/img/budget.png'
 import news from '../../assets/img/news.png'
+import readme from '../../assets/img/readme.png'
 import Carousel from "react-bootstrap/Carousel";
 require("dotenv").config();
 
@@ -61,6 +62,14 @@ function Carousels() {
             <p>News By The Map is a web application that brings you the latest news based off of a searched location using Google Maps.</p>
             <h4><a href="https://github.com/adamkeyser45/newsbythemap" target="blank">Github Repo</a></h4>
             <h4><a href="https://adamkeyser45.github.io/newsbythemap/" target="blank">Deployed Link</a></h4>
+        </Carousel.Caption>.
+       </Carousel.Item>
+       <Carousel.Item>
+       <img  className="d-block w-100" src={readme}  alt='Fifth Slide'/>
+       <Carousel.Caption>
+            <h4 style={{textAlign: 'center'}}>README Generator</h4>
+            <p>This is a professional README generator. When the user invokes the application by running node index.js in the terminal, the application will run and prompt the user for information. The information will be logged into the correct format and a professional README wil be automatically generated and will appear in the root of the directory</p>
+            <h4><a href="https://github.com/melaniegilman/readMeGenerator" target="blank">Github Repo</a></h4>            
         </Carousel.Caption>.
        </Carousel.Item>
       </Carousel>
